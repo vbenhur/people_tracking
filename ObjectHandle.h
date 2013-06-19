6dd9342fc09383ea963f5c53ce19f40a739bf35a
vbenhur <vbenhur@gmail.com>
2013-06-19 11:43:13 +0200

    Repair ObjectHandle to work in matlab 2012.b (linux)

--- ObjectHandle.h
+++ ObjectHandle.h
@@ -110,8 +110,8 @@ class Collector {
 	std::list<ObjectHandle<T>*> objlist;
 public:
 	~Collector() {
-		std::list<ObjectHandle<T>*>::iterator i;
-		std::list<ObjectHandle<T>*>::iterator end= objlist.end();
+		typename std::list<ObjectHandle<T>*>::iterator i; 
+		typename std::list<ObjectHandle<T>*>::iterator end= objlist.end(); 
 		for (i= objlist.begin(); i!=end; ++i) {
 			if ((*i)->signature == *i) // check for valid signature
 				delete *i;
